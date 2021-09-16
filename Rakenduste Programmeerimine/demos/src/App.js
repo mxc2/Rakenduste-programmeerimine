30d3071ac69ccd3f4ece37508380eae8f53373e3
Marcus-indrek Simmer <marcus.indrek.simmer@gmail.com>
2021-09-16 17:19:24 +0300

    push push push....IT'S A BOI
    Teine push, demo on Intelij IDEA, demos on React

---
+++ Rakenduste Programmeerimine/demos/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div>
+      Tere
+      <Item />
+      <Item />
+    </div>
+  );
+}
+
+export default App;
---
+++ Rakenduste Programmeerimine/demos/src/components/Item.js
@@ -0,0 +1,11 @@
+function Item(){   
+    return(
+        <div>
+            <div classname="itemName">Ese nr 1</div>
+            <div classname="itemPrice">100</div>
+            <div classname="itemCategory">mobiles</div>
+       </div>
+    )
+}
+
+export default Item;
